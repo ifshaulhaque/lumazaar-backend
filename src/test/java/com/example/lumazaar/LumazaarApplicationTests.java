4761748f228d3a37ce72498b3549eb179679f942
ifshaulhaque <ifshaul.haque@gmail.com>
2024-07-15 22:03:43 +0530

    Initial Commit

---
+++ src/test/java/com/example/lumazaar/LumazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lumazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
